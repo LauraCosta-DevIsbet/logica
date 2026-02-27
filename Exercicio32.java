209afc8b75f985572ab9f1f34f3e90574ed218b2
instrutor-edmar <ssg.s24ultra@gmail.com>
2026-02-27 20:38:14 +0000

    Resolução da questão 32

--- Exercicio32.java
+++ Exercicio32.java
@@ -3,10 +3,18 @@
 import java.util.Scanner;
 
 public class Exercicio32 {
+    public static int somar (int numero){
+        if(numero == 0) return 0;
+        return numero+somar(numero-1);
+    }
+    
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in); 
 
-		//escreva seu código aqui
+		System.out.print("Informe um número: ");
+		int num = input.nextInt();
+		
+		System.out.println("O somatório de 1 a " + num + somar(num));
 		
 		input.close();
 	}
